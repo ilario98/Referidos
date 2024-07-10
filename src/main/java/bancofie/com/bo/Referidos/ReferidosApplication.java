6c4964830c55db9740b8b7c965cd69dc7a4fec2e
ilario98 <65923751+ilario98@users.noreply.github.com>
2024-07-09 23:39:15 -0400

    Add files via upload

---
+++ src/main/java/bancofie/com/bo/Referidos/ReferidosApplication.java
@@ -0,0 +1,13 @@
+package bancofie.com.bo.Referidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bancofie/com/bo/Referidos/ReferidosApplicationTests.java
@@ -0,0 +1,13 @@
+package bancofie.com.bo.Referidos;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
