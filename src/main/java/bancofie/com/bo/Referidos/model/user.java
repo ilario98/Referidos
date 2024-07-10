487d2c116a4eeb8aafabc4675e628cc1acd24b8b
ilario98 <dsebastian1998@hotmail.com>
2024-07-09 23:45:07 -0400

    model user

---
+++ src/main/java/bancofie/com/bo/Referidos/model/user.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bancofie.com.bo.Referidos.model;
+
+/**
+ *
+ * @author HP
+ */
+public class user {
+    
+}
